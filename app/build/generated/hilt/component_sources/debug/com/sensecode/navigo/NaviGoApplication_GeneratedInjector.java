ed751f745712515e8bc278edb254a6c43dd585cf
akshatddyl <akshatdhondiyal14@gmail.com>
2026-04-03 08:57:08 +0530

    feat:Added  mapshare upload

---
+++ app/build/generated/hilt/component_sources/debug/com/sensecode/navigo/NaviGoApplication_GeneratedInjector.java
@@ -0,0 +1,17 @@
+package com.sensecode.navigo;
+
+import dagger.hilt.InstallIn;
+import dagger.hilt.codegen.OriginatingElement;
+import dagger.hilt.components.SingletonComponent;
+import dagger.hilt.internal.GeneratedEntryPoint;
+import javax.annotation.processing.Generated;
+
+@OriginatingElement(
+    topLevelClass = NaviGoApplication.class
+)
+@GeneratedEntryPoint
+@InstallIn(SingletonComponent.class)
+@Generated("dagger.hilt.android.processor.internal.androidentrypoint.InjectorEntryPointGenerator")
+public interface NaviGoApplication_GeneratedInjector {
+  void injectNaviGoApplication(NaviGoApplication naviGoApplication);
+}
